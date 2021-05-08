a5e2832852ff53a34d053d69cf2364ddd9653d9c
Milena <milenahikari11@gmail.com>
2021-05-08 14:30:40 -0300

    Ajustando header responsivo

--- src/components/Header/Profile.tsx
+++ src/components/Header/Profile.tsx
@@ -1,15 +1,20 @@
 import { Avatar, Box, Flex, Text } from "@chakra-ui/react";
 
-export function Profile() {
+interface ProfileProps {
+  showProfileData?: boolean;
+}
+
+export function Profile({showProfileData}: ProfileProps) {
   return (
     <Flex align="center">
-      <Box mr="4" textAlign="right">
-        <Text>Milena Hikari</Text>
-        <Text color="gray.300" fontSize="small">
-          milenahikari11@gmail.com
-        </Text>
-      </Box>
-
+      {showProfileData && (
+        <Box mr="4" textAlign="right">
+          <Text>Milena Hikari</Text>
+          <Text color="gray.300" fontSize="small">
+            milenahikari11@gmail.com
+          </Text>
+        </Box>
+      )}
       <Avatar size="md" name="Milena Hikari" src="https://github.com/milenahikari.png"/>
     </Flex>
   );
